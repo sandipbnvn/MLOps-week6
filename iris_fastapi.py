2f29d8816998a3c70a6d6c1db062085b7a9ac6d2
sandipbnvn <sandipbnvnhjv@gmail.com>
2025-07-26 11:13:28 +0530

    checking stg

--- iris_fastapi.py
+++ iris_fastapi.py
@@ -22,7 +22,7 @@ class IrisInput(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Iris Classifier API!"}
+    return {"message": "Welcome to the Iris Classifier API!v2"}
 
 @app.get("/health")
 def health_check():
